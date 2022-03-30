c2cd500276d3cceba6fac3c6768c68c6f09149ce
Romain Laval <romain.laval000@gmail.com>
2022-03-30 11:39:10 +0200

    reglages

--- src/MenuProfil.rb
+++ src/MenuProfil.rb
@@ -8,29 +8,32 @@ class MenuProfil
     @listeProfil
     #private_class_method :new
 
-    def initialize()
+    def initialize(fenetre)
 
-        @builder = Gtk::Builder.new()
-        @builder.add_from_file("../asset/glade/menuProfil.glade")
-        main = @builder.get_object('profilWindow')
+      @builder = Gtk::Builder.new()
+      @builder.add_from_file("../asset/glade/menuProfil.glade")
+      main = @builder.get_object('profilWindow')
 
-        mainColor = Gdk::RGBA::parse("#003049")
-        secondColor = Gdk::RGBA::parse("#00507a")
-        main.override_background_color(:'normal', mainColor)
+      mainColor = Gdk::RGBA::parse("#003049")
+      secondColor = Gdk::RGBA::parse("#00507a")
+      main.override_background_color(:'normal', mainColor)
 
-        listbox = @builder.get_object('listbox')
-        listbox.override_background_color(:'normal', secondColor)
-        main.set_title("Menu profil")
+      listbox = @builder.get_object('listbox')
+      listbox.override_background_color(:'normal', secondColor)
+      main.set_title("Menu profil")
 
 
-        @ListeProfil = Array.new()
-        @ListeButton = Array.new()
-        @ListeLabels = Array.new()
-        @files = Dir.glob("../save/*")
+      @ListeProfil = Array.new()
+      @ListeButton = Array.new()
+      @ListeLabels = Array.new()
+      @files = Dir.glob("../save/*")
 
-        @files.each{ |n| @ListeProfil.push(File.basename(n,"*"))}
-        puts(@ListeProfil)
-        @ListeProfil.each{ |n|
+      @files.each do |n| 
+        @ListeProfil.push(File.basename(n,"*"))
+      end
+
+      puts(@ListeProfil)
+      @ListeProfil.each do |n|
         label = Gtk::Label.new(n)
         @ListeLabels.push(label)
 
@@ -40,64 +43,82 @@ class MenuProfil
         button.relief = :none
         @ListeButton.push(button)
 
-
-        #save
-        newProfilButton = @builder.get_object("newProfilButton")
-        newProfilLabel = @builder.get_object("newProfilLabel")
-        newProfilButton.signal_connect "clicked" do
+      end
 
 
-          #popup
-          popup = Gtk::Window.new("First example")
-          popup.show
-          popup.set_title "// Edit //"
+      #save
+      newProfilButton = @builder.get_object("newProfilButton")
+      newProfilLabel = @builder.get_object("newProfilLabel")
+      newProfilButton.signal_connect "clicked" do
+        puts "nouveau profil"
+        showNewProfilPopup()
+      end
 
+      @ListeLabels.each do |n| 
+        n.name = "BTNLVL"
+      end
 
-          box = Gtk::Box.new(:vertical, 10) 
-
-          text = Gtk::Entry.new
-          text.set_text "Entrez votre nom"
-          box.add(text)
-
-          button = Gtk::Button.new "Valider"
-          button.signal_connect "clicked" do
-              File.new("../save/gilbert.yml","w+")
-          end
-          box.add(button)
-
-          popup.add box
-          popup.show_all
-
+      @ListeButton.each do |n| 
+        n.signal_connect "clicked" do |_widget|
+          puts "Hello World!!"
         end
+      end
+
+      provider = Gtk::CssProvider.new()
+      provider.load(data: <<-CSS)
+      #BTNLVL{
+          font-family: "Pixellari";
+          font-size: 65px;
+      }
+      CSS
+      Gtk::StyleContext.add_provider_for_screen(Gdk::Screen.default, provider, Gtk::StyleProvider::PRIORITY_APPLICATION)
+
+      @ListeButton.each{ |n| listbox.add(n)}
 
 
-        }
-        @ListeLabels.each{ |n| n.name = "BTNLVL"}
+      button = Gtk::Button.new(:label => "Say hello")
+      button.signal_connect "clicked" do |_widget|
+        puts "Hello World!!"
+      end
 
-        @ListeButton.each{ |n| n.signal_connect "clicked" do |_widget|
-            puts "Hello World!!"
-          end}
+      #main.add(button)
+      main.signal_connect("delete-event") do |_widget| 
+        Gtk.main_quit
+      end
 
-        provider = Gtk::CssProvider.new()
-        provider.load(data: <<-CSS)
-        #BTNLVL{
-            font-family: "Pixellari";
-            font-size: 65px;
-        }
-        CSS
-        Gtk::StyleContext.add_provider_for_screen(Gdk::Screen.default, provider, Gtk::StyleProvider::PRIORITY_APPLICATION)
+      main.show_all
 
-        # @ListeButton.each{ |n| listbox.add(n)}
+    end
 
 
-button = Gtk::Button.new(:label => "Say hello")
-button.signal_connect "clicked" do |_widget|
-  puts "Hello World!!"
-end
+    def showNewProfilPopup()
+      #popup
+      popup = Gtk::Window.new("First example")
+      popup.show
+      popup.set_title "// Edit //"
 
-#main.add(button)
-main.signal_connect("delete-event") { |_widget| Gtk.main_quit }
-main.show_all
+
+      box = Gtk::Box.new(:vertical, 10) 
+
+      text = Gtk::Entry.new
+      text.set_text "Entrez votre nom"
+
+      box.add(text)
+
+      button = Gtk::Button.new "Valider"
+      button.signal_connect "clicked" do
+        createProfil(text.text)
+        popup.close()
+      end
+      box.add(button)
+
+      popup.add box
+      popup.show_all
+    end
+
+    def createProfil(name)
+      puts "creation profil de " + name
+      File.new("../save/"+name+".yml","w+")
     end
 
 end
