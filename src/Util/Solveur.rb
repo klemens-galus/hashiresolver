599ab6086cc8ee00bf24f2aea00614fbe5be6cf3
Romain Laval <romain.laval000@gmail.com>
2022-04-07 11:59:08 +0200

    Methode 2

--- src/Util/Solveur.rb
+++ src/Util/Solveur.rb
@@ -8,6 +8,8 @@ class Solveur
 
   def analyser_grille
     return if tester_methode1 == true
+
+    pas_aides if tester_methode2 == false
   end
 
   #
@@ -18,12 +20,37 @@ class Solveur
     @grille.liste_iles.each do |ile|
       if ile.numero == 4 && ile.get_liste_voisins.length == 2 && !ile.est_complete?
         # une aide à déja été demandé pour ce cas => affichage de l'ile concernée
-        if @gui.aide_label.text == MethodesTexte::METHODE1_DIFFICILE
+        if @gui.aide_label.text == MethodesTexte::METHODE1_NORMALE
           @gui.aide_label.set_text(MethodesTexte::METHODE1_FACILE)
           ile.afficher_aide
         # Blocage dans l'état aide facile (pour ne pas réafficher l'aide difficile)
         elsif @gui.aide_label.text != MethodesTexte::METHODE1_FACILE
-          @gui.aide_label.set_text(MethodesTexte::METHODE1_DIFFICILE)
+          @gui.aide_label.set_text(MethodesTexte::METHODE1_NORMALE)
+        else
+          # Réaffichage de la cellule
+          ile.afficher_aide
+        end
+        return true
+      end
+    end
+
+    false
+  end
+
+  #
+  # Si une ile de 6 à trois voisins, tout relier par des ponts doubles
+  #
+  def tester_methode2
+    # Verification des iles
+    @grille.liste_iles.each do |ile|
+      if ile.numero == 6 && ile.get_liste_voisins.length == 3 && !ile.est_complete?
+        # une aide à déja été demandé pour ce cas => affichage de l'ile concernée
+        if @gui.aide_label.text == MethodesTexte::METHODE2_NORMALE
+          @gui.aide_label.set_text(MethodesTexte::METHODE2_FACILE)
+          ile.afficher_aide
+        # Blocage dans l'état aide facile (pour ne pas réafficher l'aide difficile)
+        elsif @gui.aide_label.text != MethodesTexte::METHODE2_FACILE
+          @gui.aide_label.set_text(MethodesTexte::METHODE2_NORMALE)
         else
           # Réaffichage de la cellule
           ile.afficher_aide
@@ -31,34 +58,12 @@ class Solveur
 
         return true
       end
-
-      return false
     end
 
-    # Aucune ile ne correspond à la méthode
-    @gui.aide_label.set_text(MethodesTexte::AUCUNE_AIDE)
+    false
   end
 
-  def tester_methode2
-    # Verification des iles
-    @grille.liste_iles.each do |ile|
-      if ile.numero == 4 && ile.get_liste_voisins.length == 2 && !ile.est_complete?
-        # une aide à déja été demandé pour ce cas => affichage de l'ile concernée
-        if @gui.aide_label.text == MethodesTexte::METHODE1_DIFFICILE
-          @gui.aide_label.set_text(MethodesTexte::METHODE1_FACILE)
-          ile.afficher_aide
-        # Blocage dans l'état aide facile (pour ne pas réafficher l'aide difficile)
-        elsif @gui.aide_label.text != MethodesTexte::METHODE1_FACILE
-          @gui.aide_label.set_text(MethodesTexte::METHODE1_DIFFICILE)
-        else
-          # Réaffichage de la cellule
-          ile.afficher_aide
-        end
-
-        return
-      end
-    end
-
+  def pas_aides
     # Aucune ile ne correspond à la méthode
     @gui.aide_label.set_text(MethodesTexte::AUCUNE_AIDE)
   end
