800b3078d44a39074fd09b212ef07874ba501426
Jules Beuvier <jbeuvierpr@gmail.com>
2022-04-07 11:39:14 +0200

    update texte

--- src/UI/MethodesTexte.rb
+++ src/UI/MethodesTexte.rb
@@ -2,7 +2,55 @@ module MethodesTexte
 
   AUCUNE_AIDE = "Aucune aide n'a été trouvé"
 
-  METHODE1_DIFFICILE = "Une île de 4 avec deux voisines peut être reliré à l'aide de ponts doubles"
-  METHODE1_FACILE = "L'ile qui peut etre relié avec des ponts doubles à été montrée"
+  METHODE1_NORMALE = "Une île de 4 avec deux voisines peut être reliée à l'aide de ponts doubles"
+  METHODE1_FACILE = "L'ile qui peut être reliée avec deux ponts doubles à été montrée"
+
+  METHODE2_NORMALE = "Une île de 6 avec trois voisines peut être reliée à l'aide de ponts doubles"
+  METHODE2_FACILE = "L'île qui peut être reliée avec trois ponts doubles à été montrée"
+
+  METHODE3_NORMALE = "Une île de 8 avec quatre voisines peut être reliée à l'aide de ponts doubles"
+  METHODE3_FACILE = "L'île qui peut être reliée avec quatre ponts doubles à été montrée"
+
+  METHODE4_NORMALE = "Une île avec une seule voisine peut être reliée avec cette dernière"
+  METHODE4_FACILE = "L'île avec une seule voisines qui peut être reliée à été montrée"
+
+  METHODE5_NORMALE = "Une île de 3 avec deux voisines peut être reliée à l'aide de pont(s) simple(s)"
+  METHODE5_FACILE = "L'île qui peut être reliée avec deux ponts simples à été montrée"
+
+  METHODE6_NORMALE = "Une île de 5 avec trois voisines peut être reliée à l'aide de pont(s) simple(s)"
+  METHODE6_FACILE = "L'île qui peut être reliée avec trois ponts simples à été montrée"
+
+  METHODE7_NORMALE = "Une île de 7 avec quatre voisines peut être reliée à l'aide de pont(s) simple(s)"
+  METHODE7_FACILE = "L'île qui peut être reliée avec quatre ponts simples à été montrée"
+
+  METHODE8_NORMALE = "Une île de 3 avec deux voisines dont une île de 1 peut reliée tous ses ponts"
+  METHODE8_FACILE = "L'île de 3 qui peut avoir tous ses ponts reliée à été montrée"
+
+  METHODE9_NORMALE = "Une île de 5 avec trois voisines dont une île de 1 peut reliée tous ses ponts"
+  METHODE9_FACILE = "L'île de 5 qui peut avoir tous ses ponts reliée à été montrée"
+
+  METHODE10_NORMALE = "Une île de 7 avec quatre voisines dont une île de 1 peut reliée tous ses ponts"
+  METHODE10_FACILE = "L'île de 7 qui peut avoir tous ses ponts reliée à été montrée"
+
+  METHODE11_NORMALE = "Une île de 4 avec trois voisines dont deux îles de 1 peut reliée tous ses ponts"
+  METHODE11_FACILE = "L'île de 4 qui peut avoir tous ses ponts reliée à été montrée"
+
+  METHODE12_NORMALE = "Une île de 6 avec quatre voisines dont une île de 1 peut reliée tous ses ponts"
+  METHODE12_FACILE = "L'île de 6 qui peut avoir tous ses ponts reliée à été montrée"
+
+  METHODE13_NORMALE = "Une île de 1 ne peut pas être reliée à une autre île de 1 pour éviter d'être isolée"
+  METHODE13_FACILE = "Les îles de 1 isolées ont été montrées"
+
+  METHODE14_NORMALE = "Une île de 2 ne peut pas être reliée à une autre île de 2 avec tous ses ponts pour éviter d'être isolée"
+  METHODE14_FACILE = "Les îles de 2 isolées ont été montrées"
+
+  METHODE15_NORMALE = "Une île de 2 avec deux voisines de 1 ne peut pas reliée tous ses ponts à ces dernières pour éviter d'être isolée"
+  METHODE15_FACILE = "L'île de 2 isolée a été montrée"
+
+  METHODE16_NORMALE = "Une île de 3 avec deux voisines dont une de 1 et une de 2 ne peut pas reliée tous ses ponts à ces dernières pour éviter d'être isolée"
+  METHODE16_FACILE = "L'île de 3 isolée a été montrée"
+
+  METHODE17_NORMALE = "Un segment d'îles complétées ne peut pas finir sur une île de 1 pour éviter d'être isolé"
+  METHODE17_FACILE = "Le segment d'îles isolées à été montré"
 
 end
