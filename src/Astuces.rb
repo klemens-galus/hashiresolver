d0733b2d66bd255ed3c2876e125eadba61206a00
Romain Laval <romain.laval000@gmail.com>
2022-04-07 15:37:47 +0200

    commentaires

--- src/Astuces.rb
+++ src/Astuces.rb
@@ -3,11 +3,23 @@ require 'gdk3'
 require_relative 'MainMenu'
 require_relative './UI/AppColors'
 
+#
+# Menu qui contient des textes pour la compréhension du jeu
+#
 class Astuces
+
+  # @pseudo Uttilisé pour repasser le pseudo du joueur au menu principal en cas de retour
+  # @numero_courant Numero actuel de la page affiché
+  # @window Fenetre qui contient ce menu
+  # @builder Builder Gtk pour implementer le fichier Glade
+  # @pages_text Array contenant les différents textes du tuto
+  # @text_buffer_page Buffer uttilisé pour l'affichage du text dans la text_view
+  # @text_view_page TextView uttilisé pour l'affichage du text
 
   def initialize(fenetre, pseudo)
     @pseudo = pseudo
     @numero_courant = 0
+
     build_interface(fenetre)
     populate_liste_pages
     setup_text_view
@@ -18,6 +30,11 @@ class Astuces
     @window.show_all
   end
 
+  #
+  # Chargement des composants graphiques Gtk dans la fenetre
+  #
+  # @param [Gtk::Window] fenetre Fenetre dans laquelle le menu va s'afficher
+  #
   def build_interface(fenetre)
     @builder = Gtk::Builder.new
     @builder.add_from_file('../asset/glade/Astuces.glade')
@@ -30,22 +47,38 @@ class Astuces
     @window.set_title('Astuces')
   end
 
+  #
+  # Charges les pages du tutos présentent dans le dossier textes des assets
+  #
   def populate_liste_pages
+    # Récuperation des fichiers correspondants aux pages du tuto
     fichier_pages = Dir.glob('../asset/textes/*')
 
     @pages_text = []
 
+    # Lecture des fichiers
     fichier_pages.each do |fichier_page|
       @pages_text << File.open(fichier_page).read
     end
   end
 
+  #
+  # Affiche le texte correspondants à la page dans la text_view
+  #
+  # @param [int] numero Numéro de la page
+  #
   def afficher_page(numero)
+    # Affichage du text
     @text_buffer_page.set_text(@pages_text[numero])
+
+    # Mise a jour du compteur de pages
     pages_num_label = @builder.get_object('pagesNumLabel')
     pages_num_label.set_text("Page #{numero + 1}/#{@pages_text.length}")
   end
 
+  #
+  # Applicatio du css aux composants Gtk
+  #
   def apply_css
     provider = Gtk::CssProvider.new
     provider.load(data: <<-CSS)
@@ -61,13 +94,23 @@ class Astuces
     @text_view_page.override_background_color(:normal, AppColors::SECOND_COLOR)
   end
 
+  #
+  # Initialisation du text_view et de son buffer
+  #
   def setup_text_view
     @text_view_page = @builder.get_object('viewtext')
+
+    # Création d'un buffer pour la transimition du texte
     @text_buffer_page = Gtk::TextBuffer.new
     @text_view_page.set_buffer @text_buffer_page
+
+    # nom pour le css
     @text_view_page.name = 'text_view_page'
   end
 
+  #
+  # Gestion des signaux
+  #
   def connect_signals
     retour_btn = @builder.get_object('retourBtn')
     retour_btn.signal_connect('clicked') do
@@ -83,6 +126,7 @@ class Astuces
       @builder.get_object('retourImage').set_from_file('../asset/images/return.png')
     end
 
+    # Affichage de la page précédente
     page_precedente_btn = @builder.get_object('backBtn')
     page_precedente_btn.signal_connect('clicked') do
       if @numero_courant.positive?
@@ -99,6 +143,7 @@ class Astuces
       @builder.get_object('backImg').set_from_file('../asset/images/boutons/gauche/gauche.png')
     end
 
+    # Affichage de la page suivante
     page_suivante_btn = @builder.get_object('forwBtn')
     page_suivante_btn.signal_connect('clicked') do
       if @numero_courant < @pages_text.length - 1
@@ -115,9 +160,12 @@ class Astuces
       @builder.get_object('forwImg').set_from_file('../asset/images/boutons/droite/droite.png')
     end
 
-    @window.signal_connect('delete-event') { |_widget| Gtk.main_quit }
+    @window.signal_connect('delete-event') { Gtk.main_quit }
   end
 
+  #
+  # Methode qui vide la fenêtre. A utiliser avant de leguer la fenêtre à un nouveau menu
+  #
   def clear_window
     @window.remove(@builder.get_object('astuces'))
   end
